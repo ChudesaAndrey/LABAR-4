5243d939a4287867d9bc2750c2450936cf1e6fbc
ChudesaAndrey <125434378+ChudesaAndrey@users.noreply.github.com>
2023-03-24 09:53:57 +1000

    LABAR-4

--- laba№4/main.c
+++ laba№4/main.c
@@ -3,7 +3,7 @@
 #include <math.h>
 
 int main(){
-    double s;
+    float s;
     int a, b, c, p, r;
     scanf("%d %d %d", &a, &b, &c);
     if (check(a, b, c)){
@@ -11,8 +11,9 @@ int main(){
         s = square(a, b, c);
         s = sqrt(s);
         printf("perimetr %d \n", p);
-        printf("square %lf", s);
+        printf("square %f", s);
         }
     else {printf("Error");
     }
     return 0;
+}
